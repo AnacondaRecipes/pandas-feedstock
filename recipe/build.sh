07eecfb1606ef942c03747d7d82587c3208d2be8
Steven <scrass@anaconda.com>
2024-02-19 16:08:29 -0700

    add --no-deps and --no-build-isolation

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,4 +12,4 @@ mkdir builddir
 $PYTHON -m build --wheel --no-isolation --skip-dependency-check \
     -Cbuilddir=builddir \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
-$PYTHON -m pip install dist/pandas*.whl
+$PYTHON -m pip install dist/pandas*.whl --no-deps --no-build-isolation
